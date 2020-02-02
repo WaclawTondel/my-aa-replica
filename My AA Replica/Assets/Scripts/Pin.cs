ef3cc0c544fc9764b456d97cc20ed9301d486df0
Wacław Tondel <wawatondel@gmail.com>
2020-02-02 17:44:20 +0100

    Added Pins sticking to the Circle

---
+++ My AA Replica/Assets/Scripts/Pin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+    private bool isPinned = false;
+    public float velocity = 20f;
+    public Rigidbody2D rb;
+
+    void Update() {
+        if (!isPinned) {
+            rb.MovePosition(rb.position + Vector2.up * velocity * Time.deltaTime);
+        }
+    }
+
+    void OnTriggerEnter2D (Collider2D collider) {
+        if(collider.tag == "circle") {
+            transform.SetParent(collider.transform);
+            isPinned = true;
+        }
+    }
+}
