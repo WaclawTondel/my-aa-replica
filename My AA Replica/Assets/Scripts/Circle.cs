35bad7e0f1ca2965b970d12ccbcc2c73b52be040
Wacław Tondel <wawatondel@gmail.com>
2020-02-02 16:18:07 +0100

    Initial commit with rotating circle

---
+++ My AA Replica/Assets/Scripts/Circle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+    public float angularVelocity = 100f;
+
+    void Update() {
+        transform.Rotate(0f, 0f, angularVelocity * Time.deltaTime);
+    }
+}
