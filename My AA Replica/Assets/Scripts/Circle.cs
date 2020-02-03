75ee8c362db04fcbc5c417544f5f86a48b4d21ee
Wacław Tondel <wawatondel@gmail.com>
2020-02-03 02:01:40 +0100

    Gravity implementation
    Buggy Implementation of gravity

--- My AA Replica/Assets/Scripts/Circle.cs
+++ My AA Replica/Assets/Scripts/Circle.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Circle : MonoBehaviour
 {
-    public float angularVelocity = 100f;
+    public static float angularVelocity = 100f;
+    public static float additionalMassAngularVelocity = 60f;
+    public static List<Pin> pins = new List<Pin>();
 
     void Update() 
     {
-        transform.Rotate(0f, 0f, angularVelocity * Time.deltaTime);
+        float GravityMultiplayer = 0;
+        foreach(Pin pin in pins)
+        {
+                GravityMultiplayer += pin.CalculatePinGravityMultiplayer();
+        }
+        transform.Rotate(0f, 0f, (angularVelocity + additionalMassAngularVelocity * GravityMultiplayer) * Time.deltaTime);
+        Debug.LogWarning(GravityMultiplayer);
+        Debug.LogWarning(angularVelocity + additionalMassAngularVelocity * GravityMultiplayer);
     }
 }
--- My AA Replica/Assets/Scripts/Pin.cs
+++ My AA Replica/Assets/Scripts/Pin.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class Pin : MonoBehaviour
 {
     private bool isPinned = false;
+
     public float velocity = 20f;
     public Rigidbody2D rb;
+
+    private float TimeMultiplayer = 0;
 
     void Update() 
     {
         if (!isPinned)
         {
             rb.MovePosition(rb.position + Vector2.up * velocity * Time.deltaTime);
+        }else if (TimeMultiplayer < 1)
+        {
+            TimeMultiplayer += Time.deltaTime / 5;
+        }else
+        {
+            TimeMultiplayer = 1;
         }
     }
 
@@ -22,9 +31,25 @@ public class Pin : MonoBehaviour
         {
             transform.SetParent(collider.transform);
             isPinned = true;
+            Circle.pins.Add(this);
         } else if(collider.tag == "pin") 
         {
             FindObjectOfType<GameManager>().EndGame();
         }
+    }
+
+
+    public float CalculatePinGravityMultiplayer()
+    {
+        return (Mathf.Cos(GetPinRotationInRadians())) * TimeMultiplayer;
+    }
+
+    private float GetPinRotation()
+    {
+        return this.transform.rotation.eulerAngles.z;
+    }
+    private float GetPinRotationInRadians()
+    {
+        return (this.GetPinRotation() * Mathf.PI) / 180f;
     }
 }
